be04e7390f15801573d4e1aa1bf4bd740d087c47
cimerson <42168758+cimerson@users.noreply.github.com>
2018-08-07 11:49:21 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GroupComponent } from './bender/bender-group.component';
+import { BenderComponent } from './bender/bender.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupComponent,
+    BenderComponent
   ],
   imports: [
     BrowserModule
